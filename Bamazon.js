4bdc5f2ce6f21917bdf30a4d8440ca9d78cce760
Msingh91 <manishsingh@Manishs-MacBook-Pro.local>
2017-06-04 19:59:18 -0500

    ok

--- Bamazon.js
+++ Bamazon.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql")
+var inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -16,7 +17,81 @@ connection.connect(function(err) {
 	console.log("connected as id" + connection.threadId)
 })
 
+
+
 connection.query("SELECT * FROM products", function(err,res) {
 	if (err) throw err;
-	console.log(res)
+	for (var i = 0; i < res.length; i++) {
+		console.log(res[i].item_id + "|" + res[i].product_name + "| Price: $" + res[i].price + "| Quantity: " + res[i].stock_quantity)
+	}
+
 })
+
+
+ var purchase = function () {
+
+ 	connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+ 	inquirer.prompt([
+ 	{
+    name: "item",
+    type: "rawlist",
+    choices: function(){
+    	choiceArray = []
+    	for (var i = 0; i < res.length; i++) {
+    		choiceArray.push(res[i].product_name)
+
+    	}
+    	return choiceArray
+    },
+    message: "Welcome to Super Mart Services! Please select the item you would like to      purchase!"
+},
+{
+
+	name: "number",
+	type:"input",
+	message: "How many would you like to purchase?"
+}
+  ]).then(function(answer) {
+  	var chosenItem;
+  	for (var i = 0; i < res.length; i++) {
+  		if (res[i].product_name === answer.item) {
+  		chosenItem = res[i];
+  		console.log(chosenItem)
+  	}
+  	}
+  	
+
+  	if (answer.number <= parseInt(chosenItem.stock_quantity)) {
+  		connection.query("UPDATE products SET ? WHERE?", [{
+  			stock_quantity:(parseInt(chosenItem.stock_quantity) - parseInt(answer.number))
+
+  		}, {
+  			item_id: chosenItem.item_id
+  		}], function(error) {
+  			if (error) throw err;
+  			console.log("Quantity remaining: " + (chosenItem.stock_quantity-answer.number))
+  			console.log("Total purchase price: $" + (chosenItem.price*answer.number))
+  			console.log("Order placed succesfully!")
+  		})
+
+  	}
+
+  	else {
+  		console.log("Sorry there is insufficient quantity")
+  	}
+
+
+  })
+})
+
+}
+purchase()
+
+
+
+
+
+
+
